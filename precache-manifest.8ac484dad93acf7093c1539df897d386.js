6459b9404dc8f03dbdc16932cb5dc3365a09c9bc
Patrik Masiar <patrik@kontentino.com>
2019-10-18 22:17:14 +0200

    Updates

---
+++ precache-manifest.8ac484dad93acf7093c1539df897d386.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5495cd3d51aa9635a4433e29891b2559",
+    "url": "/react-mini-rocket-game/index.html"
+  },
+  {
+    "revision": "e3d5bbf8b8ee30beb5ac",
+    "url": "/react-mini-rocket-game/static/css/main.52ddfa44.chunk.css"
+  },
+  {
+    "revision": "8a202093cffc9b7eb6db",
+    "url": "/react-mini-rocket-game/static/js/2.005d2c56.chunk.js"
+  },
+  {
+    "revision": "e3d5bbf8b8ee30beb5ac",
+    "url": "/react-mini-rocket-game/static/js/main.9f979065.chunk.js"
+  },
+  {
+    "revision": "60d051a92fdce628f8bc",
+    "url": "/react-mini-rocket-game/static/js/runtime-main.81a4de54.js"
+  },
+  {
+    "revision": "884f8fa0c922ff00cf6e92f260aca798",
+    "url": "/react-mini-rocket-game/static/media/rocketone.884f8fa0.png"
+  },
+  {
+    "revision": "3b15ccbda8e544f47d47208c3de9e533",
+    "url": "/react-mini-rocket-game/static/media/rockettwo.3b15ccbd.png"
+  }
+]);
