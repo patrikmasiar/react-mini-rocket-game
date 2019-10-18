dde1ed85e51a65b40bdd6a10be2fd06934d966bb
Patrik Masiar <patrik@kontentino.com>
2019-10-18 22:33:57 +0200

    start handler

--- src/containers/Menu/index.js
+++ src/containers/Menu/index.js
@@ -1,26 +1,45 @@
 import React from 'react';
 import style from './style.module.css';
 import SpaceShipSelector from '../../components/SpaceShipSelector';
+import { AppConsumer } from '../../store';
 
-const Menu = () => {
+const Menu = () => (
+    <AppConsumer>
+        {value => {
+            const onStart = () => {
+                if (value.spaceshipId === null) {
+                    alert("Please, select spaceship!");
+                    return;
+                }
 
-    return (
-        <div className={style.wrapper}>
-            <span className={style.title}>Select your Spaceship</span>
-            <SpaceShipSelector />
-            <input
-                className={style.input}
-                placeholder="Your nickname"
-                required
-            />
-            <button
-                type="button"
-                className={style.button}
-            >
-                START!
-            </button>
-        </div>
-    );
-};
+                if (value.username.length === 0) {
+                    alert("Please, insert your nickname!");
+                    return;
+                }
+
+                console.log(value);
+            }
+
+            return (
+                <div className={style.wrapper}>
+                    <span className={style.title}>Select your Spaceship</span>
+                    <SpaceShipSelector />
+                    <input
+                        className={style.input}
+                        placeholder="Your nickname"
+                        onChange={e => value.setUsername(e.target.value)}
+                    />
+                    <button
+                        type="button"
+                        className={style.button}
+                        onClick={onStart}
+                    >
+                        START!
+                    </button>
+                </div>
+            );
+        }}
+    </AppConsumer>
+);
 
 export default Menu;
